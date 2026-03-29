5fc0229256c5073a013a4adbd44b8135a4306576
mixa1na <borshik0258@gmail.com>
2026-03-29 16:21:09 +0300

    Initial commit

---
+++ ContactTests.cs
@@ -0,0 +1,50 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using System.Collections.Generic;
+
+namespace Klimova_EHU
+{
+    [TestFixture]
+    public class ContactTests : BaseTest
+    {
+        [Test]
+        public void Test4_VerifyContactFormSubmission()
+        {
+            driver.Navigate().GoToUrl("https://en.ehu.lt/contact/");
+
+            var emailElement = driver.FindElement(By.XPath("//*[contains(text(), 'franciskscarynacr@gmail.com')]"));
+            Assert.That(emailElement.Displayed, Is.True, "Email not displayed");
+            Assert.That(emailElement.Text, Contains.Substring("franciskscarynacr@gmail.com"),
+                "Email address is incorrect or not visible");
+
+            var phoneLTElement = driver.FindElement(By.XPath("//*[contains(text(), '+370 68 771365')]"));
+            Assert.That(phoneLTElement.Displayed, Is.True, "Lithuanian phone number not displayed");
+            Assert.That(phoneLTElement.Text, Contains.Substring("+370 68 771365"),
+                "Lithuanian phone number is incorrect or not visible");
+
+            var phoneBYElement = driver.FindElement(By.XPath("//*[contains(text(), '+375 29 5781488')]"));
+            Assert.That(phoneBYElement.Displayed, Is.True, "Belarusian phone number not displayed");
+            Assert.That(phoneBYElement.Text, Contains.Substring("+375 29 5781488"),
+                "Belarusian phone number is incorrect or not visible");
+
+            var socialLinks = new Dictionary<string, string>
+            {
+                { "Facebook", "https://www.facebook.com/groups/434978221124539/" },
+                { "Telegram", "https://t.me/skaryna_cultural_route" },
+                { "VK", "https://vk.com/public203605228" }
+            };
+
+            foreach (var social in socialLinks)
+            {
+                var linkElement = driver.FindElement(By.XPath($"//a[contains(@href, '{social.Value}')]"));
+                Assert.That(linkElement.Displayed, Is.True, $"{social.Key} link not displayed");
+                Assert.That(linkElement.GetAttribute("href"), Is.EqualTo(social.Value),
+                    $"{social.Key} link URL is incorrect");
+            }
+
+            var contactSections = driver.FindElements(By.CssSelector(".contact-info, .contacts, .address"));
+            Assert.That(contactSections.Count, Is.GreaterThan(0),
+                "Contact information sections not found on the page");
+        }
+    }
+}
---
+++ SearchTests.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+
+namespace Klimova_EHU
+{
+    [TestFixture]
+    public class SearchTests : BaseTest
+    {
+        [Test]
+        public void Test2_VerifySearchFunctionality()
+        {
+            driver.Navigate().GoToUrl("https://en.ehu.lt/");
+
+            var searchInput = driver.FindElement(By.CssSelector("input[type='search'], input[name='s']"));
+            searchInput.Clear();
+            searchInput.SendKeys("study programs");
+
+            var searchButton = driver.FindElement(By.CssSelector("button[type='submit'], input[type='submit']"));
+            searchButton.Click();
+
+            Assert.That(driver.Url, Does.Contain("/?s=study+programs"),
+                "Search query not found in URL");
+
+            var searchResults = driver.FindElements(By.CssSelector("article, .search-result, .post"));
+            Assert.That(searchResults.Count, Is.GreaterThan(0),
+                "No search results found");
+
+            var resultLinks = driver.FindElements(By.CssSelector("a"));
+            bool hasStudyProgramsLink = false;
+            foreach (var link in resultLinks)
+            {
+                if (link.Text.Contains("study program", StringComparison.OrdinalIgnoreCase) ||
+                    link.GetAttribute("href").Contains("study-program", StringComparison.OrdinalIgnoreCase))
+                {
+                    hasStudyProgramsLink = true;
+                    break;
+                }
+            }
+            Assert.That(hasStudyProgramsLink, Is.True,
+                "No study programs related links found in search results");
+        }
+    }
+}
